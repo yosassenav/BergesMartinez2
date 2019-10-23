26c208985f33d881ddf709dc3520557de78c3ebe
Vanessa-Mtz <160300006@ucaribe.edu.mx>
2019-10-22 19:33:43 -0500

    feature/agregando nueva clase Recordatorio

---
+++ clases/Recordatorio.cs
@@ -0,0 +1,100 @@
+
+
+public class Recordatorio
+{
+   
+   // Atributos de la clase Recordatorio
+    public string  Titulo;
+    public string  Materia;
+    public bool  Estado;
+    public string  Descripcion;
+    public int Prioridad;
+    public date Fecha;
+    public time Hora;
+
+    //Constructor de la clase
+    public Recordatorio()
+    {
+    }
+
+    public void SetTitulo(string Titulo)
+    {
+        
+    }
+
+    public void GetTitulo(string Titulo)
+    {
+        
+    }
+
+    public void SetMateria(string Materia)
+    {
+        
+    }
+
+    public void GetMateria(string Materia)
+    {
+        
+    }
+
+    public void SetDescripcion(string Descripcion)
+    {
+        
+    }
+
+    public void GetDescripcion(string Descripcion)
+    {
+        
+    }
+
+    public void SetFecha(date Fecha)
+    {
+        
+    }
+
+    public void GetFecha(date Fecha)
+    {
+        
+    }
+
+    public void SetHora(time Hora)
+    {
+        
+    }
+
+    public void GetHora(time Hora)
+    {
+        
+    }
+
+    public void SetEstado(bool Estado)
+    {
+        
+    }
+
+    public void GetEstado(bool Estado)
+    {
+        
+    }
+
+    public void SetPrioridad(int Prioridad)
+    {
+        
+    }
+
+    public void GetPrioridad(int Prioridad)
+    {
+        
+    }
+
+
+    /*public int MyAutoImplementedProperty { get; set; }
+
+    private int myPropertyVar;
+    
+    public int MyProperty
+    {
+            get { return myPropertyVar; }
+            set { myPropertyVar = value; }
+    } */
+}
