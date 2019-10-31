4a87592c27f1cdeaab25de92c389c55d0348b47e
Regina del Pilar Berges Portillo <170300066@ucaribe.edu.mx>
2019-10-31 17:09:43 -0500

    Actualización Logger.cs

--- clases/Logger.cs
+++ clases/Logger.cs
@@ -1,19 +1,35 @@
+using System;
 
-public class Login{
-private static Login user = null;
-
-private Login{
-
+namespace Proyecto
+{
+    //Clase Singleton
+    public class Logger
+    {
+        //Instancia única del singleton
+        private static Logger instancia = null;
+        //static int contador = 0;  
+        //Constructor privado para degar la creación de una instancia de esta clase afuera
+        private Logger() {
+            //Para comprobar la cantidad de instancias que se pueden crear (1)
+            //contador++;  
+            //Console.WriteLine("Instancia " + contador); 
+        }
+        
+        //Para crear una única instancia
+        public static Logger Instanciar() 
+        {
+            if (instancia == null) {
+                instancia = new Logger();
+            }
+            return instancia;
+        }
+        
+        public void Log(string mensaje){   
+            
+            Console.WriteLine($"[{DateTime.Now}]: {mensaje }");
+        }
+    }
+    
+    
 }
 
-private synchronized static void createInstance() {
-		if (user == null) {
-			user = new Login();
-		}
-	}
-
-	public static Login getInstance() {
-		createInstance();
-		return user;
-	}
-}
